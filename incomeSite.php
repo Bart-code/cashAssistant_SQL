80e5224d37542b1805a3fdf7c3405bda2ce52ffa
Bartlomiej <bartlomiej.w.programista@gmail.com>
2021-05-09 19:29:51 +0200

    Feature/Modal_v1

--- incomeSite.php
+++ incomeSite.php
@@ -27,6 +27,7 @@
 			$incomeComment=$_POST['incomeComment'];
 			$sqlRequest="INSERT INTO incomes VALUES ( NULL, '$userId', '$categoryId','$incomeAmount', '$incomeDate', '$incomeComment' )";
 			mysqli_query($dataBaseConnect,$sqlRequest);
+			echo "<script> $('#myModal').modal('show');</script>";
 		}
 		else echo "WRONG !";
 	}
@@ -101,6 +102,26 @@
 		<h1 > Cash Assistant </h1>
 	</header>
 	<div class="container bg-container">
+		<!-- Modal -->
+			<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			  <div class="modal-dialog" role="document">
+				<div class="modal-content">
+				  <div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					  <span aria-hidden="true">&times;</span>
+					</button>
+				  </div>
+				  <div class="modal-body">
+					...
+				  </div>
+				  <div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				  </div>
+				</div>
+			  </div>
+			</div>
 		<div class=" mainPanel col-md-10 col-lg-8 p-4 mt-4">
 			<form method="post">
 				<div class="row">	
@@ -151,7 +172,7 @@
 						</a>
 					</div>
 					<div class="col-sm ">
-						<input class="btn buttonsStyle" type="submit" value="Submit"/>
+						<input class="btn buttonsStyle" type="submit" value="Submit" data-toggle="modal" data-target="#exampleModal"/>
 					</div>
 				</div>
 			</form>
