0e00b883068209dc5a3bf1b031751d38060df2b7
Bart-code <bartlomiej.w.programista@gmail.com>
2021-05-13 17:42:27 +0200

    Feature/Showing table with name o categories and Ids

--- balanceSite.php
+++ balanceSite.php
@@ -1,4 +1,66 @@
-<!DOCTYPE HTML>
+<?php
+	session_start();
+	if( ! isset($_SESSION['loggingSuccesfull']) || $_SESSION['loggingSuccesfull'] != true )
+	{
+		header('Location: index.php');
+		exit();
+	}
+	
+	else
+	{
+		if(isset($_POST['timeSelector']))
+		{
+			$userId=$_SESSION['loggedUserId'];
+			require_once "connect.php";
+			$dataBaseConnect = new mysqli($host,$db_user,$db_password,$db_name);
+			
+			//fill incomes category name matrix
+			$sqlRequest="SELECT incomes_category_assigned_to_users.id,
+			incomes_category_assigned_to_users.name		
+			FROM incomes_category_assigned_to_users 
+			WHERE incomes_category_assigned_to_users.user_id='$userId'";
+			if( $result=mysqli_query($dataBaseConnect,$sqlRequest))
+			{
+				if($result->num_rows)
+				{
+					$rowsIncomesCount=$result->num_rows;
+					for($i=0;$i<$rowsIncomesCount;$i++)
+					{
+						$row = $result->fetch_assoc();
+						$incomesCategoryIdMatrix[$i] = $row['id'];
+						$incomesCategoryNameMatrix[$i] = $row['name'];
+					}
+				}
+				else $_SESSION['balanceError']="Something gone wrong";
+			}
+			else $_SESSION['balanceError']="Something gone wrong";
+			
+			//fill expenses category name matrix
+			$sqlRequest="SELECT expenses_category_assigned_to_users.id,
+			expenses_category_assigned_to_users.name		
+			FROM expenses_category_assigned_to_users 
+			WHERE expenses_category_assigned_to_users.user_id='$userId'";
+			if( $result=mysqli_query($dataBaseConnect,$sqlRequest))
+			{
+				if($result->num_rows)
+				{
+					$rowsExpensesCount=$result->num_rows;
+					for($i=0;$i<$rowsExpensesCount;$i++)
+					{
+						$row = $result->fetch_assoc();
+						$expensesCategoryIdMatrix[$i] = $row['id'];
+						$expensesCategoryNameMatrix[$i] = $row['name'];
+					}
+					$showTable=true;
+				}
+				else $_SESSION['balanceError']="Something gone wrong";
+			}
+			else $_SESSION['balanceError']="Something gone wrong";
+		}
+		
+	}
+?>
+
 <html lang="pl">
 <head>
 	<meta charset="utf-8" />
@@ -65,7 +127,7 @@
 			
 			document.getElementById("downDateBorder").value = downBorder;
 			document.getElementById("topDateBorder").value = topBorder;
-			document.getElementById("tables").style.display='none';
+			//document.getElementById("tables").style.display='none';
 		}
 		
 		function setMaxDay( month )
@@ -99,103 +161,120 @@
 	
 	<div class="container bg-container">
 		<div class=" mainPanel col-md-10 col-lg-8 p-4 mt-4">
-			<div class="row">
-					<div class="col-md-5 col-sm-10 mx-auto">			
+			<form method="post">
+				<div class="row">
+						<div class="col-md-5 col-sm-10 mx-auto">			
+							<div class="row pb-1">
+								Time borders:
+							</div>
+							<div class="row pb-2">
+								<select class="textField" name="timeSelector" id="timeBordersSelector" onchange="setTimeBorders()">
+									<option>Current month</option>
+									<option>Previous month</option>
+									<option>Current year</option>
+									<option>Custom </option>
+								</select>
+							</div>
+						</div>
+						<div class="col-md-5 col-sm-10 mx-auto">
+						</div>
 						<div class="row pb-1">
-							Time borders:
-						</div>
-						<div class="row pb-2">
-							<select class="textField" id="timeBordersSelector" onchange="setTimeBorders()">
-								<option>Current month</option>
-								<option>Previous month</option>
-								<option>Current year</option>
-								<option>Custom </option>
-							</select>
-						</div>
-					</div>
-					<div class="col-md-5 col-sm-10 mx-auto">
-					</div>
-					<div class="row pb-1">
-						<div class="col-md-5 col-sm-10 mx-auto">
-							<div class="row pb-1">
-								Down border: 
+							<div class="col-md-5 col-sm-10 mx-auto">
+								<div class="row pb-1">
+									Down border: 
+								</div>
+								<div class="row pb-2">
+									<input type="date" name="downBorder" class="textField timeBorders" id="downDateBorder" />
+								</div>
 							</div>
-							<div class="row pb-2">
-								<input type="date" class="textField timeBorders" id="downDateBorder" />
+							<div class="col-md-5 col-sm-10 mx-auto">
+								<div class="row pb-1">
+									Top border: 
+								</div>
+								<div class="row pb-2">
+									<input type="date" name="topBorder" class="textField timeBorders" id="topDateBorder" />
+								</div>
 							</div>
 						</div>
-						<div class="col-md-5 col-sm-10 mx-auto">
-							<div class="row pb-1">
-								Top border: 
-							</div>
-							<div class="row pb-2">
-								<input type="date" class="textField timeBorders" id="topDateBorder" />
-							</div>
-						</div>
-					</div>
-					<div class="row pb-1 mt-3 ">
+						<div class="row pb-1 mt-3 ">
 							<div class="w-50px">
-								<button type="button" class= "btn buttonsStyle" onclick="showTables()">
-									Show balance
-								</button>
+								<input type="submit" class= "btn buttonsStyle" value="Show balance" />
 							</div>
-					</div>
-			</div>
-		
+						</div>
+				</div>
+			</form>
 			<div class="row mt-4"  id="tables">
 				<div class="col-md-5 col-sm-10 mx-auto">
 					<div class="row">
-						Incomes:
+						<?php if(isset($showTable) && $showTable==true)
+							{
+								echo "Incomes:";
+							}
+						?>
 					</div>
 					<div class="row">
-						<table>
-							<thead>
-								<tr>
-									<th>Category</th>
-									<th>Sum amount zł</th>
-								</tr>
-							</thead>
-							<tr> 
-								<td>Category1</td> <td>10</td>
-							</tr>
-							<tr> 
-								<td>Category2</td> <td>100</td>
-							</tr>
-							<tr> 
-								<td>Category3</td> <td>1000</td>
-							</tr>
-						</table>
+						
+							<?php
+									if(isset( $_SESSION['balanceError']) ) echo "<option>". $_SESSION['balanceError']."</option>";
+									elseif(isset($showTable) && $showTable==true)
+									{
+										echo "<table>
+											<thead>
+												<tr>
+													<th>Category</th>
+													<th>Sum amount zł</th>
+												</tr>
+											</thead>";
+										for($i=0;$i<$rowsIncomesCount;$i++)
+										{
+											echo "<tr> 
+													<td>".$incomesCategoryNameMatrix[$i]."</td>
+													<td>".$incomesCategoryIdMatrix[$i]."</td>
+												</tr>";
+										}
+										echo "</table>";
+									}
+							?>
+						
 					</div>
 				</div>
 				<div class="col-md-5 col-sm-10 mx-auto">
 					<div class="row">
-						Expenses:
+						<?php if(isset($showTable) && $showTable==true)
+							{
+								echo "Expenses:";
+							}
+						?>
 					</div>
 					<div class="row">
-							<table>
+						<?php
+							if(isset( $_SESSION['balanceError']) ) echo "<option>". $_SESSION['balanceError']."</option>";
+							elseif(isset($showTable) && $showTable==true)
+							{
+								echo "<table>
 									<thead>
 										<tr>
 											<th>Category</th>
 											<th>Sum amount zł</th>
 										</tr>
-									</thead>
-								<tr> 
-									<td>Category1</td> <td>10</td>
-								</tr>
-								<tr> 
-									<td>Category2</td> <td>100</td>
-								</tr>
-								<tr> 
-									<td>Category3</td> <td>1000</td>
-								</tr>
-							</table>
+									</thead>";
+								for($i=0;$i<$rowsExpensesCount;$i++)
+								{
+									echo "<tr> 
+											<td>".$expensesCategoryNameMatrix[$i]."</td>
+											<td>".$expensesCategoryIdMatrix[$i]."</td>
+										</tr>";
+								}
+								echo "</table>";
+							}
+						?>
 					</div>
 				</div>
 			</div>
 
 			<div class="row mt-5">
 				<div class="col-sm">
-					<a class="btn buttonsStyle" href="mainSite.html" role="button">
+					<a class="btn buttonsStyle" href="mainSite.php" role="button">
 						<i class="icon-left-big"></i> Back
 					</a>
 				</div>
